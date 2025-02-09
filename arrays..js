3aadb27009b56dad4611784e50c4cfff97ec8272
anastewa <katejka208@gmail.com>
2025-02-09 21:01:54 +0500

    Add files via upload

---
+++ arrays..js
@@ -0,0 +1,10 @@
+
+const room = ["кровать", "окно", "шкаф" , 'мягкий гусь'];
+console.log(room[0]); //индекс
+console.log(room[1]); 
+console.log(room[3]); 
+console.log(room); //вывод масива
+console.log(room.length); //количество
+room [3] = "куроми"; //замена
+console.log(room);//вывод
+
