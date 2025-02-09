4b21e814a8e8b765077f102bb307a338dcee5473
anastewa <katejka208@gmail.com>
2025-02-09 21:03:39 +0500

    Update arrays..js

--- arrays..js
+++ arrays..js
@@ -1,5 +1,8 @@
 
-const room = ["кровать", "окно", "шкаф" , 'мягкий гусь'];
+const room = ["кровать",
+              "окно", 
+              "шкаф" , 
+              'мягкий гусь'];
 console.log(room[0]); //индекс
 console.log(room[1]); 
 console.log(room[3]); 
